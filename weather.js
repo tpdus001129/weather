1ab9bb8db6fa1845e6cbeaf8070eec49f4210b19
tpdus001129 <tpdus001129@gamil.com>
2023-10-05 15:15:05 +0900

    https

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const key = "qCx5RGcAT2eALs6QQxxVvedfFHTLcsKw";
 
 const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
