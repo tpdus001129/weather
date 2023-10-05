6fe26829b344a6495b22ae3affdcdde2e5234e8d
tpdus001129 <tpdus001129@gamil.com>
2023-10-05 14:29:08 +0900

    1차 완료

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const cityForm = document.querySelector("form");
 
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
 const updateCity = async (city) => {
   const cityInfo = await getCity(city);
   const weather = await getWeather(cityInfo.Key);
@@ -10,12 +13,30 @@ const updateCity = async (city) => {
   };
 };
 
+const updateUI = (data) => {
+  const cityInfo = data.cityInfo;
+  const weather = data.weather;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>`;
+
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
   // 가운데 city는 <input type="text" name="city" /> 의 name 이름
   const city = cityForm.city.value;
 
   updateCity(city).then((data) => {
-    console.log(data);
+    // console.log(data);
+    updateUI(data);
   });
 });
